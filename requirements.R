1ebf4b46189831ec3940086cc861dddcebfd1b26
datacamp-content-creator <32279578+datacamp-content-creator@users.noreply.github.com>
2019-03-30 10:16:29 +0100

    Update requirements.R from editor

--- requirements.R
+++ requirements.R
@@ -2,14 +2,14 @@ library(remotes)
 
 ##### Install specific package versions from CRAN #####
 
-# install_version("ggplot2", "3.0.0")
-# install_version("dplyr", "0.7.5")
-# install_version("tidyr", "0.8.1")
-# install_version("purrr", "0.2.5")
+install_version("ggplot2", "3.0.0")
+install_version("dplyr", "0.7.5")
+install_version("tidyr", "0.8.1")
+install_version("purrr", "0.2.5")
 
 ##### Install packages in development from GitHub #####
 
-# install_github("nicholasehamilton/ggtern")
+install_github("nicholasehamilton/ggtern")
 
 ##### Install packages from Bioconductor
 
